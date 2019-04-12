eeba7deb8f09b922e9a35568d2e0adfcdf604ff0
joeanello <joe.a@modernroboticsinc.com>
2019-04-12 06:13:22 +0000

    Release Candidate: FusionRT v1.3.0a RC1 Build-1  [ Dev: FusionServer-v1.3.0a-RC1  RT: FRT-v1.3.0RC1b01 ]

---
+++ etc/scanports.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#===============================================================================
+# ports.sh - Shell script to identify the networking ports
+#===============================================================================
+#
+# This script is used to identify the networking ports and assign Environment
+# variables to point at the ports being used for wired Ethernet, wireless
+# Ethernet, and wireless access point.
+#
+#-------------------------------------------------------------------------------
+# Revision History:
+#   01-Apr-2019 <jwa> - Original Version
+#
+#===============================================================================
+
+
+#==============================================================================
+# Start of Script Mainline
+#
+# Get the interface configurations for all the ports and only keep those with
+# MAC Addresses (HWaddr)
+#
+_PORTS=(`ifconfig -a | grep "HWaddr"`)
+
+ENET="NULL"
+WIFI="NULL"
+WAPT="NULL"
+
+let i=0
+while [ ! -z ${_PORTS[$i]} ]; do
+
+	MACOUI=`echo ${_PORTS[i+4]} | cut -d: -f1-3`
+	PORTTYPE=`echo ${_PORTS[$i]} | cut -c1-3`
+	if [[ ${MACOUI} == "b8:27:eb" || ${MACOUI} == "dc:a6:32" ]]; then
+
+		# Internal Assets
+		if [[ ${PORTTYPE} == "eth" ]]; then
+			if [[ ${ENET} == "NULL" ]]; then
+				ENET=${_PORTS[$i]}
+			fi
+		elif [[ ${PORTTYPE} == "wla" ]]; then
+			if [[ ${WAPT} == "NULL" ]]; then
+				WAPT=${_PORTS[$i]}
+			fi
+		fi
+	else
+		# External Assets
+		if [[ ${PORTTYPE} == "wla" ]]; then
+			if [[ ${WIFI} == "NULL" ]]; then
+				WIFI=${_PORTS[$i]}
+			fi
+		fi
+	fi
+	
+	let i=i+5
+done
+
+#-------------------------------------------------------------------------------
+# Since you can't easily export Environment Variables to the PARENT process,
+# we'll write the stuff to a file and can then 'source' the file in other 
+# scripts to get these variables to make decisions based on.
+#
+# We'll use /usr/Fusion/etc/portassigns as the file, and it gets rewritten each
+# time we run this process
+#
+echo "export ENET=${ENET}"  > /usr/Fusion/etc/portassigns
+echo "export WAPT=${WAPT}" >> /usr/Fusion/etc/portassigns
+echo "export WIFI=${WIFI}" >> /usr/Fusion/etc/portassigns
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ etc/wlanConnect.py
@@ -0,0 +1,87 @@
+#===============================================================================
+# 11-April-2019 <jwa> -- This python script is depreciated.  Nothing calls it,
+#    the Fusion Server takes care of managing the wireless connections directly,
+#    and the new rc.local processing takes care of assigning the utilization of
+#    of the wireless (and wired) assets.  
+#
+#    In short, we don't need this right now but I'm keeping it for reference.
+#################################################################################
+
+import sys 
+import os
+import argparse
+import traceback
+import commands
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-c',
+                    action='store_true',
+                    dest='connect',
+                    default=False,
+                    help='Connect to a network')
+                    
+parser.add_argument('-d',
+                    action='store_true',
+                    dest='disconnect',
+                    default=False,
+                    help='Disconnect from a network')
+                    
+parser.add_argument('-l',
+                    action='store_true',
+                    dest='listNetworks',
+                    default=False,
+                    help='List all found networks')
+                    
+parser.add_argument('-n',
+                    action='store',
+                    dest='network_ssid',
+                    default=None,
+                    help='SSID of network to connect to')
+                    
+parser.add_argument('-p',
+                    action='store',
+                    dest='network_pass',
+                    default=None,
+                    help='Network password')
+                    
+results = parser.parse_args()
+
+try:
+    if(results.listNetworks != False):
+        ssid_list = ""
+        sig_strength = ""
+        encryption = ""
+        
+        iwlist = commands.getstatusoutput("sudo iwlist wlan1 scan")[1]
+        lines = iwlist.split(os.linesep)
+        
+        for line in lines:
+            if "Signal level=" in line:
+                sig_strength += line[line.index("Signal level=")+13:] + ';'
+            if "ESSID:" in line:
+                ssid_list += line[line.index("ESSID:")+6:].replace('"', '') + ';'
+            if "Encryption key:" in line:
+                encryption += line[line.index("Encryption key:")+15:] + ';'
+        
+        print ssid_list
+        print sig_strength
+        print encryption
+        
+    elif((results.connect == True) and (results.network_ssid != None)):
+        results.network_ssid.replace('"', "")
+        if(results.network_pass != None):
+            commands.getstatusoutput("sudo cp /usr/Fusion/etc/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf")
+            commands.getstatusoutput("sudo wpa_passphrase " + results.network_ssid + " " + results.network_pass + " >> /etc/wpa_supplicant/wpa_supplicant.conf")
+        else:
+            commands.getstatusoutput("sudo cp /usr/Fusion/etc/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf")
+            commands.getstatusoutput("sudo echo 'network={\nssid=" + results.network_ssid + "\nkey_mgmt=NONE\n}' >> /etc/wpa_supplicant/wpa_supplicant.conf")
+        commands.getstatusoutput("sudo wpa_cli reconfigure")
+    
+    elif(results.disconnect == True):
+        commands.getstatusoutput("sudo cp /usr/Fusion/etc/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf")
+        commands.getstatusoutput("sudo wpa_cli reconfigure")
+        
+except:
+    print traceback.format_exc(1)
+    
