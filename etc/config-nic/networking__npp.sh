09dcbdcbeaa8ae12c8c950b7fd7432890cf268db
joeanello <joe.a@modernroboticsinc.com>
2019-05-07 14:50:08 +0000

    Release Candidate: FusionRT v1.3.2a RC1 Build-01 (Semia)  [ Dev: fusion-semia  RT: FRT-v1.3.2aRC1b01-Semia ]

---
+++ etc/config-nic/networking__npp.sh
@@ -0,0 +1,20 @@
+#===============================================================================
+# networking__npp.sh - FusionOS/NIC networking control file
+#-------------------------------------------------------------------------------
+# 13-Apr-2019 <jwa> - Original Debian Jessie Configuration File
+#
+# Used when Internal WiFi Asset is a Network Interface (NIC)
+#     Runtime target: /etcdefault/networking
+#-------------------------------------------------------------------------------
+
+# Configuration for networking init script being run during
+# the boot sequence
+
+# Set to 'no' to skip interfaces configuration on boot
+#CONFIGURE_INTERFACES=yes
+
+# Don't configure these interfaces. Shell wildcards supported/
+#EXCLUDE_INTERFACES=
+
+# Set to 'yes' to enable additional verbosity
+#VERBOSE=no
