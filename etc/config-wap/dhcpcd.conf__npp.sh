a77b76e59b2452027c80800d2505ae8a975d3c94
joeanello <joe.a@modernroboticsinc.com>
2020-05-12 14:39:37 +0000

    Release Candidate: FusionRT v1.4.0X WorkingIssue-3 Build-02  [ Dev: master  RT: FRT-v1.4.0Xwi3b03 ]

---
+++ etc/config-wap/dhcpcd.conf__npp.sh
@@ -0,0 +1,10 @@
+#===============================================================================
+# dhcpcd.conf__npp.sh - FusionOS/WAP dhcpcd.conf configuration file
+#-------------------------------------------------------------------------------
+# 13-Apr-2019 <jwa> - Original Debian Jessie Configuration File
+#
+# Used when Internal WiFi Asset is a Wireless Access Point (WAP)
+#   Runtime target: /etc/dhcpcd.conf
+#===============================================================================
+
+denyinterfaces wlan0
